e4729986a9193525acc1e03eb82333f8fd50417a
IDea Factory <dev-team@ideafactory.fr>
2018-05-28 12:25:38 +0200

    Utilisation de l'interface Civilite

--- src/app/components/signupform/signupform.component.ts
+++ src/app/components/signupform/signupform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Civilite } from './../../models/civilite';
 
 @Component({
   selector: 'app-signupform',
@@ -9,14 +10,14 @@ export class SignupformComponent implements OnInit {
   /**
    * Tableau des civilités à afficher dans le formulaire
    */
-  public civilite: string[];
+  public civilite: Civilite[];
 
   constructor() {
     this.civilite = new Array();
 
-    this.civilite.push('Mademoiselle');
-    this.civilite.push('Madame');
-    this.civilite.push('Monsieur');
+    this.civilite.push({id: 1, libelle: 'Mademoiselle'});
+    this.civilite.push({id: 2, libelle: 'Madame'});
+    this.civilite.push({id: 3, libelle: 'Monsieur'});
    }
 
   ngOnInit() {
